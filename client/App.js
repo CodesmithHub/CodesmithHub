a1f3cda63d23cbab45c95e4afb2723791f0adb9c
Jon Perera <jon.perera@hotmail.com>
2017-08-24 19:10:35 -0700

    boilerplate complete

---
+++ client/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+* A counter button: tap the button to increase the count.
+*/
+class App extends React.Component {
+ constructor() {
+   super();
+   this.state = {};
+ }
+
+ render() {
+   return (
+     <div>Dis be da App</div>
+   );
+ }
+}
+export default App;
