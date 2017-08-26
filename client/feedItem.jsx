b9e427315d86a34b60afb775b91b08bff9819950
David Sally <david@Davids-MacBook-Pro.local>
2017-08-25 17:21:21 -0700

    basic news feed, updated components

---
+++ client/feedItem.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FeedItem extends Component {
+  
+  render() {
+    return (
+      <li className="feed-item">
+        <img src={this.props.imgURL} alt={this.props.username + "'s picture"} />
+        <br />
+        <span>{this.props.username}    {this.props.message}</span>
+      </li>
+    );
+  }
+}
+
+export default FeedItem;
