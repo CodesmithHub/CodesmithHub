ed524262522dad3d1ca0a2ac0c0c04fae1733f36
Balal Zuhair <balalzuhair@balals-mbp.gateway.pace.com>
2017-12-10 14:33:36 -0800

    changed into pure component

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainPage from './mainPage.jsx';
 import LogIn from './login.js';
 import SignUp from './signup.js';
@@ -7,22 +7,16 @@ import Directory from './directory.jsx';
 import ProfilePage from './profile.jsx';
 import NewsFeed from './newsFeed.jsx';
 
-class App extends React.Component {
-
-
-  render() {
-    return (
-      <div>Dis be da App
-        <BrowserRouter>
-          <div>
-            <Route exact path="/" component={LogIn} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route path="/main" component={MainPage} />
-          </div>
-        </BrowserRouter>
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={LogIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route path="/main" component={MainPage} />
       </div>
-    );
-  }
-}
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
